58dd9f19a9ab1883f2d2e96aa549a0de7e8e7270
imam chanafi <iman.chanafi@corp.bri.co.id>
2022-10-21 09:14:26 +0700

    push logo

--- src/layout/nav/NavLogo.js
+++ src/layout/nav/NavLogo.js
@@ -12,7 +12,7 @@ const NavLogo = () => {
           Or added via css to provide different ones for different color themes
          */}
         {/* <div className="img" /> */}
-        <img src="/img/logo/logo-tpm-landscape-1.png" alt="logo" width={250} height={45} />
+        <img src="/img/logo/logo-tpm-landscape-1.png" alt="logo" width={"300px"} height={"45px"} />
       </Link>
     </div>
   );
